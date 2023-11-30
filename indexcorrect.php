41ce0accbbca21df662af540766325833b22f826
REMYA_K_RAJAN <117816591+remya007@users.noreply.github.com>
2023-11-30 21:56:43 +0530

    Create indexcorrect.php

---
+++ indexcorrect.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form</title>
+</head>
+<body>
+        <form>
+    <link rel="stylesheet" href="index.css">
+    <h1>MEDSHOPPE</h1>
+            <button>
+                <a href="pharmacistlogin.php">pharmacist</a></button>&nbsp;
+        
+                <button>   
+    
+                <a href="patientlogin.php">Patient</a></button>&nbsp;
+
+                 <button> 
+                <a href="log.php">Admin</a></button>
+    
+        </form>
+</body>
+</html>
